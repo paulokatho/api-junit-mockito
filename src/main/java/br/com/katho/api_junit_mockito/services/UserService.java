04b358c8fb63156bc6ae3c642e80dba2cd8e6e53
paulokatho <pkatho@gmail.com>
2025-06-26 21:50:16 +0100

    feat: alteracao do User para UserEntity na interface de UserService

--- src/main/java/br/com/katho/api_junit_mockito/services/UserService.java
+++ src/main/java/br/com/katho/api_junit_mockito/services/UserService.java
@@ -1,8 +1,8 @@
 package br.com.katho.api_junit_mockito.services;
 
-import br.com.katho.api_junit_mockito.domain.User;
+import br.com.katho.api_junit_mockito.domain.UserEntity;
 
 public interface UserService {
 
-    User findById(Integer id);
+    UserEntity findById(Integer id);
 }
