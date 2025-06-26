ccb5cbfa68b335a68ab9f913c37a0a95e695314f
paulokatho <pkatho@gmail.com>
2025-06-26 21:49:32 +0100

    feat: classe que trata o retorno de status NOT_FOUND

---
+++ src/main/java/br/com/katho/api_junit_mockito/resource/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.katho.api_junit_mockito.resource.exceptions;
+
+import br.com.katho.api_junit_mockito.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(
+            ObjectNotFoundException e
+            , HttpServletRequest request) {
+
+        StandardError error = new StandardError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                request.getRequestURI() // You can set the path if needed
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
