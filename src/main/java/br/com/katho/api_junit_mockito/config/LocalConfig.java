d8e5808eb1f3fc35e3b3b30b62a815a57a233a5d
paulokatho <pkatho@gmail.com>
2025-06-26 21:47:37 +0100

    feat: Acrescentando @PostConstruct para poder retornar os valores quando a tabela de UserEntity for criada

--- src/main/java/br/com/katho/api_junit_mockito/config/LocalConfig.java
+++ src/main/java/br/com/katho/api_junit_mockito/config/LocalConfig.java
@@ -2,21 +2,22 @@ package br.com.katho.api_junit_mockito.config;
 
 import br.com.katho.api_junit_mockito.domain.UserEntity;
 import br.com.katho.api_junit_mockito.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
-//@Configuration
-//@Profile("local")
+@Configuration
+@Profile("local")
 public class LocalConfig {
 
-    //@Autowired
+    @Autowired
     private UserRepository userRepository;
 
     //@Bean
+    @PostConstruct
     public void startDBH2() {
         UserEntity u1 = new UserEntity(1, "Katho Metal", "katho@email.com", "123");
         UserEntity u2 = new UserEntity(2, "Kirk", "hamet@email.com", "456");
