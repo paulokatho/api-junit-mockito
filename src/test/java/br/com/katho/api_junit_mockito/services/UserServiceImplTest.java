7c72cb68282b809592dd1b9f49ebf6fb26b3031d
paulokatho <pkatho@gmail.com>
2025-07-01 14:30:49 +0100

    feat: teste para classe user service metodo create com sucesso

--- src/test/java/br/com/katho/api_junit_mockito/services/UserServiceImplTest.java
+++ src/test/java/br/com/katho/api_junit_mockito/services/UserServiceImplTest.java
@@ -95,11 +95,23 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        UserEntity response = service.create(userDTO);
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(UserEntity.class, response.getClass());
+        Assertions.assertEquals(ID, response.getId());
+        Assertions.assertEquals(NAME, response.getName());
+        Assertions.assertEquals(EMAIL, response.getEmail());
+        Assertions.assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
     void update() {
+
+
     }
 
     @Test
